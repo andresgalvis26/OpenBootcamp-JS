54db3bd5ed1b573637296e2c253c0f655f057e61
MBP-AndresGalvis <andres.felipe99@hotmail.com>
2022-07-27 19:25:28 -0500

    Culminación EjercicioPractico 2

--- 07-Otros-Tipos-Objetos/EjerciciosPracticos/conjuntos.js
+++ 07-Otros-Tipos-Objetos/EjerciciosPracticos/conjuntos.js
@@ -16,6 +16,9 @@ const setNombresFamilia = new Set(nombresFamilia)
 console.log(setNombresFamilia)
 
 
+// Forma correcta de crear el set
+const familia = new Set(["Gorka", "Maria", "Marta", "Gorka"])
+
 // Modificando el setOriginal agregando mi nombre
 setNombresFamilia.add('Andres Galvis')
 console.log(setNombresFamilia)
